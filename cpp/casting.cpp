364425d7935ff13afe9a78aedf835b4e810b1488
nipun510 <nipunkumar510@gmail.com>
2018-11-05 13:49:10 +0530

    modification

--- cpp/casting.cpp
+++ cpp/casting.cpp
@@ -2,6 +2,20 @@
 //Consider using a run-time checked cast, such as narrow_cast<>(),
 // for conversion between numeric types
 
+class A
+{
+public:
+	virtual void fun(){}
+};
+
+class B
+{
+
+public:
+	void g(){}
+	void fun(){}
+};
+
 int main()
 {
 
@@ -14,11 +28,28 @@ int main()
 
 //static_cast converts between related types such as one pointer type 
 //to another in the same class hierarchy
+
+//It does things like implicit conversions 
+
+//static_cast performs no runtime checks. 
+//This should be used if you know that you refer to an object 
+//of a specific type at compile time, and thus a check would be unnecessary.
+	
 	int nd = static_cast<int>(d);
-	//int ns = static_cast<int>(s); compilation error invalid static_cast
+	//int ns = static_cast<int>(s); /*Ques1*/
 	int nc = static_cast<char>(d);
 	std::cout << nc << "\n";
 	std::cout << nd << "\n";
+//dynamic_cast does run-time checked conversion of pointers and references into a class hierarchy
+//It returns a null pointer if the object referred to doesn't 
+//contain the type casted to as a base class (when you cast to a reference, 
+//a bad_cast exception is thrown in that case).
+	A * aobj = new A{};
+	B * bobj = new B{};
+
+	bobj = dynamic_cast<B *>(aobj);
+	if(bobj == NULL){std::cout << "NULL\n";}
+	//bobj->fun();
 
 //reinterpret_cast handles conversions between unrelated types such as an integer to a pointer
 //or a pointer to an unrelated pointer type
@@ -28,9 +59,15 @@ int main()
 	//int∗ p2 = static_cast<int∗>(&x); // error : no implicit char* to int* conversion
 	//int *p3 = reinterpret_cast<int∗>(&x); // OK: on your head be it
 
-//dynamic_cast does run-time checked conversion
-// of pointers and references into a class hierarchy
 
-	//int val{112.2};invalid narrowing 
+
+// {} will perform only ‘‘well-behaved’’ conversions.
+// {}-initializer notation does not allow narrowing 
+//int val{112.2};invalid narrowing 
+
+/*Output
+compilation error invalid static_cast
+
+*/	
 
 }
--- cpp/class_base.cpp
+++ cpp/class_base.cpp
@@ -14,17 +14,15 @@ class derived : public base
 public:
 	derived(int val1, int val2): val{val1}, base{val2}{}
 	int val;
-	void fun() override{std::cout << "derived fun()" << std::endl;}
+	void fun() override{std::cout << "derived fun() " << val  << std::endl;}
 };
 
-void fun(base * b)
-{
-	std::cout << b->val << std::endl; // base value
-	b->fun(); // derived fun called
-}
 
 int main()
 {
 	derived d{12, 23};
-	fun(&d);
+	base * b = &d;
+
+	b->fun();
+	std::cout << b->val << std::endl;
 }
