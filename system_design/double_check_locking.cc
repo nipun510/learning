0983fd8e4cfe6f4690288a8fa357a4ab761cc7e8
Nipun kumar <nipun510@users.noreply.github.com>
2019-11-28 01:11:29 +0530

    Create double_check_locking.cc

---
+++ system_design/double_check_locking.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+/*
+In software engineering, double-checked locking (also known as "double-checked locking optimization"[1]) is a software design pattern
+ used to reduce the overhead of acquiring a lock by testing the locking criterion (the "lock hint") before acquiring the lock. 
+ Locking occurs only if the locking criterion check indicates that locking is required.
+*/
+
+bool globalStatus = false;
+std::mutex m_gs;
+
+void setGlobalStatus(){
+    std::lock_guard<std::mutex> lock(m_gs);
+    globalStatus = true; // this code has to be executed only once, however lock is acquired everytime it is called.
+}
+
+void setGlobalStatusImproved(){
+    //this is called double checked locking
+    if (not globalStatus) {
+        std::lock_guard<std::mutex> lock(m_gs);
+        if (not globalStatus) {
+            globalStatus = true;
+        }
+    }
+    std::lock_guard<std::mutex> lock(m_gs);
+    globalStatus = true; 
+}
+
+
+int main(){
+    auto l = [](){setGlobalStatus();};
+    std::thread t1(l);
+}
