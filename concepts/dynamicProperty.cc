0e7f89356454be9451a7b29b6787d505d947f3db
Nipun kumar <nipun510@users.noreply.github.com>
2020-06-19 13:17:51 +0530

    Update dynamicProperty.cc
    enhancement and commenting

--- concepts/dynamicProperty.cc
+++ concepts/dynamicProperty.cc
@@ -35,7 +35,10 @@ class value2 : public valueType{
     }
 };
 
-//fixed property
+// fixed property 
+// all the properties can only be added statically and 
+// not dynamically at run time. In below example class A has two fixed
+// properties name and adress.
 class A
 {
 public:
@@ -48,6 +51,7 @@ private:
 };
 
 //flexible dynamic property
+// Any property can be added at run time. so no check on property name.
 class B
 {
 public:
@@ -62,72 +66,89 @@ private:
 };
 
 //defined dynamic propery
-class propertyType
-{
-private:
-    static std::map<std::string, propertyType> _properties;
-    std::string _name;
-public:
-    static propertyType getProperty(const std::string & name) 
-    {
-        if (_properties.find(name) == _properties.end()){ throw std::logic_error("bad property Name");};
-        return _properties[name];
-    }
-    static hasProperty(const std::string & name)
-    {
-        return _properties.find(name) != _properties.end();
-    }
-    propertyType () {};
-    propertyType(std::string name){
-        _name = name;
-        _properties.insert({_name, *this});
-    }
-    propertyType(const propertyType & rhs){
-        _name = rhs.getName();
-    }
-    std::string getName() const { return _name; }
+// Only pre
 
-};
-std::map<std::string, propertyType> propertyType::_properties;
 
 class C
 {
-public:
-    valueType *
-    getValue(propertyType key) {
-        return values[key.getName()];
+private:
+    class property {
+    private:
+        std::string _name;
+    public:
+
+        property () {};
+        property(std::string name){
+            _name = name;  
+        }
+        property(const property & rhs){
+            _name = rhs.getName();
+        }
+        std::string getName() const { return _name; }
+    };
+    
+    static void
+    setProperties(std::vector<std::string> validPropertyNames) {
+       for (auto & name : validPropertyNames) {
+           _properties[name] = new property{name};
+       }
     }
-    void setValue(propertyType key, valueType* value) {
-        valueType *newvalue = value->clone();
-        values[key.getName()] = newvalue;
+    
+    
+public:
+    static bool
+    hasProperty(const std::string & name) {
+        return _properties.find(name) != _properties.end();
+    }
+public:
+    C (std::vector<std::string> propertyNames) {
+        setProperties(propertyNames);
+    }
+    
+    valueType *
+    getValue(const std::string &propertyName) {
+        if (hasPropery(propertyName)) {
+            return _keyValueMap[_properties[propertyName]];
+        }
+        return nullptr;
+    }
+    
+    bool 
+    setValue(const std::string &propertyName, valueType* value) {
+        if (value == nullptr || hasProperty(propertyName)) {
+            return false;
+        }
+        valueType *newValue = value->clone();
+        _keyValueMap[_properties[propertyName]] = newValue;
+        return true;
     }
 private:
-    std::map<std::string, valueType*> values;
+    std::map<property*, valueType*> _keyValueMap;
+    static std::map<std::string, property*> _properties;
 };
 
+
+std::map<std::string, propertyType> C::_properties;
 
 // Driver code 
 int main() 
 {
-    //flexible dynamic property
+    //flexible dynamic property, no check on propertyNames
+    // here it might be the case that user doesn't want garbage propery in class B.
     B b;
-    value1 val1;
-    value2 val2;
-    b.setValue("name", val1);
-    b.setValue("address", val2);
+    value1 nameValue;
+    value2 addressValue;
+    value3 garbageValue;
+    b.setValue("name", nameValue);
+    b.setValue("address", addressValue);
+    b.setValue("garbage", garbageValue);
     
     // defined dynamic propery
-    propertyType p1("name");
-    propertyType p2("address");
-    
-    C c;
-    
-    c.setValue(propertyType::getProperty("name"), &val1);
-    c.setValue(propertyType::getProperty("address"), &val2);
-    valueType *valc1 = c.getValue(propertyType::getProperty("name"));
-    std::cout << valc1->get() << "\n";
-
-    valueType *valc2 = c.getValue(propertyType::getProperty("address"));
-    std::cout << valc2->get() << "\n";
-    
+    // Only valid property names are allowed.
+    std::vector<std::string> propertyNames{"name", "address", "age"};
+    C cobj{propertyNames}; // property names for class C fixed now.
+    cobj.setValue("name", nameValue);
+    cobj.setValue("address", addressValue);
+    cobj.setValue("garbage", garbageValue); // this will fail at run time. will return false.
+    #ToDo property name check at compile time.
 } 
